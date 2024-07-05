2e56a6fe46b460ef04145b72e3368dbe4c577eb6
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-07-05 19:07:31 +0300

    fix: fixed header

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,4 +1,5 @@
 $(".header__burger").click(function() {
     $(this).toggleClass("header__burger_active")
     $(".mobile-menu").toggleClass("mobile-menu_active")
+    $("body, html").toggleClass("lock")
 })
